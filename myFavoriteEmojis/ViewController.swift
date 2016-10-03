3c4ed9ade70a82b325bc30d18fcefea7001aeba2
ZD <dalezachary4@gmail.com>
2016-10-03 08:26:01 -0400

    This is the data source and delegate stage of the table

--- myFavoriteEmojis/ViewController.swift
+++ myFavoriteEmojis/ViewController.swift
@@ -5,15 +5,54 @@
 //  Created by BHSRam3 on 9/30/16.
 //  Copyright © 2016 BHSRam3. All rights reserved.
 //
+// need to tell tables number of rows and content in those rows, will give an error until it is completely built
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    
+    @IBOutlet weak var coolTableView: UITableView!
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        coolTableView.dataSource=self
+            // used to define the table
+        coolTableView.delegate=self
+            // and contents
+        
+        
+        
+        
+        
+        
     }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+      return 100
+        
+    }
+            // ^looks for somewhwere the number of rows( an integer)^
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell=UITableViewCell()
+            //^ same as assigning a value
+        
+        cell.textLabel?.text=" 💩 hello"
+        
+        return cell
+    }
+    
+    //^ says in order what do you want in the cells
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
